39c2ca17f15f01db4d1f9f9cb351f7f4734e5058
Punar619 <101385681@georgebrown.ca>
2025-01-13 00:12:51 -0500

    lab1

---
+++ lab1.py
@@ -0,0 +1,16 @@
+# 101385681
+# Punar Sharifli
+
+array_var = [1,4,7,9]
+
+
+a,b,c,d = 1,2,3,4
+
+e = (a - (b ** c) // d + (a % c))
+
+tempature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius ". format(tempature))
+
+age = int (input("Enter your age:"))
+add_age= age + 22
+print (f"Now showing the shop items filtered by age: {add_age}")
